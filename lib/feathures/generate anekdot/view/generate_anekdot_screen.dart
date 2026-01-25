138ff98acae091439c573ebb954a140cf1dec768
Дмитрий Дехтяренко <mrdima_aysdehtyarenko@mail.ru>
2026-01-25 21:11:13 +0600

    generate anekdot in screen

--- lib/feathures/generate anekdot/view/generate_anekdot_screen.dart
+++ lib/feathures/generate anekdot/view/generate_anekdot_screen.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:category_b/core/services/show_anekdot_bottom_sheet.dart';
+import 'package:category_b/feathures/generate%20anekdot/bloc/generate_anekdot_bloc.dart';
 import 'package:category_b/feathures/generate%20anekdot/widgets/generate_anekdot_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class GenerateAnekdotScreen extends StatefulWidget {
@@ -15,6 +17,8 @@ class _GenerateAnekdotScreenState extends State<GenerateAnekdotScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final bloc = BlocProvider.of<GenerateAnekdotBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: theme.cardColor,
@@ -24,13 +28,27 @@ class _GenerateAnekdotScreenState extends State<GenerateAnekdotScreen> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: GenerateAnekdotButton(
-            text: 'Рандомный анек',
-            onPressed: () => showAnekdotBottomSheet(
-              context: context,
-              anekdotText: 'походу с анекдотами',
+          child: BlocListener<GenerateAnekdotBloc, GenerateAnekdotState>(
+            listener: (context, state) {
+              if (state is GenerateAnekdotLoaded) {
+                showAnekdotBottomSheet(
+                  context: context,
+                  anekdotText: state.anekdotText,
+                );
+              }
+            },
+            child: BlocBuilder<GenerateAnekdotBloc, GenerateAnekdotState>(
+              builder: (context, state) {
+                if (state is GenerateAnekdotLoading) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return GenerateAnekdotButton(
+                  text: 'Рандомный анек',
+                  onPressed: () => bloc.add(GenerateRandomAnekdot()),
+                  icon: Icons.auto_stories,
+                );
+              },
             ),
-            icon: Icons.auto_stories,
           ),
         ),
       ),
