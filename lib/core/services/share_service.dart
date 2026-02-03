056a2e338c754d9ec3e426e4773183bf203b965d
Дмитрий Дехтяренко <mrdima_aysdehtyarenko@mail.ru>
2026-02-03 17:55:28 +0600

    +

--- lib/core/services/share_service.dart
+++ lib/core/services/share_service.dart
@@ -2,6 +2,8 @@ import 'package:share_plus/share_plus.dart';
 
 class ShareService {
   static Future<void> shareAnekdot(String text) async {
-    await SharePlus.instance.share(ShareParams(text: text));
+    await SharePlus.instance.share(
+      ShareParams(text: '$text \n скопировано из приложения category b'),
+    );
   }
 }
