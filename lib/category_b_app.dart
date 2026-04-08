f6823fbc6586017b611bf60fb3e154e827b5675e
Дмитрий Дехтяренко <mrdima_aysdehtyarenko@mail.ru>
2026-04-08 11:19:38 +0600

    add category_b_app widget

---
+++ lib/category_b_app.dart
@@ -0,0 +1,63 @@
+import 'package:category_b/bloc/notifications/cubit/notifications_cubit.dart';
+import 'package:category_b/bloc/theme/theme_cubit.dart';
+import 'package:category_b/core/di/setup_dependencies.dart';
+import 'package:category_b/core/services/anekdot/anekdot_service_interface.dart';
+import 'package:category_b/core/services/notifications/notification_service.dart';
+import 'package:category_b/feathures/favorites/bloc/favorite_anekdots_bloc.dart';
+import 'package:category_b/feathures/generate%20anekdot/bloc/generate_anekdot_bloc.dart';
+import 'package:category_b/repositories/favorites/favorites_repository_interface.dart';
+import 'package:category_b/repositories/settings/settings_repository_interface.dart';
+import 'package:category_b/router/router.dart';
+import 'package:category_b/ui/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CategoryBApp extends StatefulWidget {
+  const CategoryBApp({super.key});
+
+  @override
+  State<CategoryBApp> createState() => _CategoryBAppState();
+}
+
+class _CategoryBAppState extends State<CategoryBApp> {
+  final _router = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => GenerateAnekdotBloc(
+            service: getIt<AnekdotServiceInterface>(),
+            favoritesRepository: getIt<FavoritesRepositoryInterface>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => FavoriteAnekdotsBloc(
+            favoritesRepository: getIt<FavoritesRepositoryInterface>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => ThemeCubit(
+            settingsRepository: getIt<SettingsRepositoryInterface>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NotificationsCubit(
+            notificationService: getIt<NotificationService>(),
+            settingsRepository: getIt<SettingsRepositoryInterface>(),
+          ),
+        ),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp.router(
+            title: 'categoryB',
+            theme: state.isDark ? darkTheme : lightTheme,
+            routerConfig: _router.config(),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,17 @@
 import 'dart:developer';
 
-import 'package:category_b/bloc/notifications/cubit/notifications_cubit.dart';
-import 'package:category_b/bloc/theme/theme_cubit.dart';
+import 'package:category_b/category_b_app.dart';
 import 'package:category_b/core/di/app_initializer.dart';
 import 'package:category_b/core/di/setup_dependencies.dart';
-import 'package:category_b/core/services/anekdot/anekdot_service_interface.dart';
-import 'package:category_b/core/services/notifications/notification_service.dart';
-import 'package:category_b/feathures/favorites/bloc/favorite_anekdots_bloc.dart';
-import 'package:category_b/feathures/generate%20anekdot/bloc/generate_anekdot_bloc.dart';
-import 'package:category_b/repositories/favorites/favorites_repository_interface.dart';
-import 'package:category_b/repositories/settings/settings_repository_interface.dart';
-import 'package:category_b/router/router.dart';
-import 'package:category_b/ui/theme/theme.dart';
+import 'package:category_b/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+
+  await dotenv.load();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
@@ -32,54 +22,4 @@ Future<void> main() async {
   log('App fully initialized');
 
   runApp(const CategoryBApp());
-}
-
-class CategoryBApp extends StatefulWidget {
-  const CategoryBApp({super.key});
-
-  @override
-  State<CategoryBApp> createState() => _CategoryBAppState();
-}
-
-class _CategoryBAppState extends State<CategoryBApp> {
-  final _router = AppRouter();
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => GenerateAnekdotBloc(
-            service: getIt<AnekdotServiceInterface>(),
-            favoritesRepository: getIt<FavoritesRepositoryInterface>(),
-          ),
-        ),
-        BlocProvider(
-          create: (context) => FavoriteAnekdotsBloc(
-            favoritesRepository: getIt<FavoritesRepositoryInterface>(),
-          ),
-        ),
-        BlocProvider(
-          create: (context) => ThemeCubit(
-            settingsRepository: getIt<SettingsRepositoryInterface>(),
-          ),
-        ),
-        BlocProvider(
-          create: (context) => NotificationsCubit(
-            notificationService: getIt<NotificationService>(),
-            settingsRepository: getIt<SettingsRepositoryInterface>(),
-          ),
-        ),
-      ],
-      child: BlocBuilder<ThemeCubit, ThemeState>(
-        builder: (context, state) {
-          return MaterialApp.router(
-            title: 'categoryB',
-            theme: state.isDark ? darkTheme : lightTheme,
-            routerConfig: _router.config(),
-          );
-        },
-      ),
-    );
-  }
 }
